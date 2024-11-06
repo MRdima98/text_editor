5d435867354b080e373390adde8e65f7e8126563
dima <dimafrunza69@gmail.com>
2024-11-06 10:33:17 +0100

    Autumn cleaning

--- main.go
+++ main.go
@@ -1,12 +1,43 @@
 package main
 
 import (
-	// #include "draw.h"
-	// #cgo LDFLAGS: -L/usr/X11/lib -lX11 -lstdc++
-	"C"
+	"fmt"
+	"github.com/BurntSushi/xgb"
+	"github.com/BurntSushi/xgb/xproto"
 )
 
 func main() {
-	C.draw()
-}
+	X, err := xgb.NewConn()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
 
+	wid, _ := xproto.NewWindowId(X)
+	screen := xproto.Setup(X).DefaultScreen(X)
+	xproto.CreateWindow(X, screen.RootDepth, wid, screen.Root,
+		0, 0, 500, 500, 0,
+		xproto.WindowClassInputOutput, screen.RootVisual,
+		xproto.CwBackPixel|xproto.CwEventMask,
+		[]uint32{ // values must be in the order defined by the protocol
+			0xffffffff,
+			xproto.EventMaskStructureNotify |
+				xproto.EventMaskKeyPress |
+				xproto.EventMaskKeyRelease})
+
+	xproto.MapWindow(X, wid)
+	for {
+		ev, xerr := X.WaitForEvent()
+		if ev == nil && xerr == nil {
+			fmt.Println("Both event and error are nil. Exiting...")
+			return
+		}
+
+		if ev != nil {
+			fmt.Printf("Event: %s\n", ev)
+		}
+		if xerr != nil {
+			fmt.Printf("Error: %s\n", xerr)
+		}
+	}
+}
--- test/go.mod
+++
@@ -1,5 +0,0 @@
-module test
-
-go 1.23.2
-
-require github.com/BurntSushi/xgb v0.0.0-20210121224620-deaf085860bc
